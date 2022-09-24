274d83ee62eb3c727a3bccea1a485d8de7bb1225
BarbaricxEric <castilloer33@yahoo.com>
2022-09-24 13:31:33 -0500

    added router

---
+++ src/components/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class ="not-found">404, page not found</h1>
+</template>
+
+<style lang="postcss" scoped>
+    
+    .not-found{
+        @apply text-center text-4xl font-bold pt-80;
+    }
+</style>
